e8b3f2702ef230f61390eb650a50a135422dd7a9
abedaton <antoine.bedaton@hotmail.fr>
2019-10-03 19:52:12 +0200

    Deleted Intro Page for now

--- app/src/main/java/com/example/passwordmanagerfixed/MainActivity.java
+++ app/src/main/java/com/example/passwordmanagerfixed/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.passwordmanagerfixed;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.os.Bundle;
@@ -14,6 +11,11 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonGen;
     private TextView password, lenPassword;
     private SeekBar seekBar;
-    private CheckBox checkSpecial, checkNum;
+    private CheckBox checkSpecial, checkNum, checkEntropy;
     private int lenPass = 1;
     private ClipboardManager myClipboard;
     private ClipData myClip;
     private ViewPager slideViewPager;
     private LinearLayout dotsLayout;
+
+    private FloatingActionButton paramsButton;
+
 
 
     @Override
@@ -42,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
         checkSpecial = findViewById(R.id.idSpecial);
         checkNum = findViewById(R.id.idNum);
         myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+
+        paramsButton = findViewById(R.id.idParams);
+        checkEntropy = findViewById(R.id.idHighEntropy);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -85,6 +93,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        paramsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button clicked :)", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        checkEntropy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int length;
+                if(checkSpecial.isChecked() && checkNum.isChecked()){
+                    length = 20;
+                }else if(checkSpecial.isChecked()){
+                    length = 21;
+
+                } else{
+                    length = 22;
+                }
+
+                if (checkEntropy.isChecked()){
+                    seekBar.setEnabled(false);
+                    lenPassword.setText("Length of password fixed: " + length);
+                }else {
+                    seekBar.setEnabled(true);
+                    lenPassword.setText("Length of password: " + seekBar.getProgress());
+                }
+
+            }
+        });
+
+
+
     }
 
 
@@ -95,17 +137,35 @@ public class MainActivity extends AppCompatActivity {
         char[] specialNoNum = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn&é'(§è!çà)-µù,;:=+/.?<>'".toCharArray();
         StringBuilder stringBuilder = new StringBuilder();
         Random random = new Random();
+
+
         for (int i = 0; i< length; i++){
             if(checkSpecial.isChecked() && checkNum.isChecked()){
+                if (checkEntropy.isChecked()){
+                    length = 20;
+                    lenPassword.setText("Length of password fixed: "+length);
+                }
                 char c = specials[random.nextInt(specials.length)];
                 stringBuilder.append(c);
             }else if(checkSpecial.isChecked()){
+                if (checkEntropy.isChecked()){
+                    length = 21;
+                    lenPassword.setText("Length of password fixed: "+length);
+                }
                 char c = specialNoNum[random.nextInt(specialNoNum.length)];
                 stringBuilder.append(c);
             } else if(checkNum.isChecked()){
+                if (checkEntropy.isChecked()){
+                    length = 22;
+                    lenPassword.setText("Length of password fixed: "+length);
+                }
                 char c = charsNum[random.nextInt(charsNum.length)];
                 stringBuilder.append(c);
             } else {
+                if (checkEntropy.isChecked()){
+                    length = 23;
+                    lenPassword.setText("Length of password fixed: "+length);
+                }
                 char c = chars[random.nextInt(chars.length)];
                 stringBuilder.append(c);
             }
